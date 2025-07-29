da6bc41b0845a0ab1116cb52dfa4db701d129a96
Ward D <w@rddeboutte.com>
2025-07-29 07:54:24 +0200

    feat: versioning, libname, templates (#7)
    * fix autoapi
    
    * shift to github versioning
    
    * rename libraries, versioning set in cargo
    
    * autodoc mock import linkapy.linkapy
    
    * add PR template
    
    * drop setuptools_scm
    
    * more options for naming

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use pyo3::prelude::*;
 mod keep_cool;
 
 #[pymodule]
-fn linkars(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn linkapy(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(keep_cool::parse_cools, m)?)?;
     Ok(())
 }
