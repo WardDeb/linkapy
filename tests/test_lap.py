7d43b7739137706dbf3011b09a2c180c7de92536
WardDeb <ward@deboutte.be>
2025-08-19 16:08:29 +0200

    test through API

--- tests/test_lap.py
+++ tests/test_lap.py
@@ -1,4 +1,27 @@
-from linkapy.parsing import Parse_scNMT
+from linkapy.parsing import Linkapy_Parser
 
-def test_Parse():
-    _a = Parse_scNMT("./", "./", chromsizes="None")
+def test_Linkapy_Parser(tmp_path):
+    reg = tmp_path / "reg.bed"
+    reg.touch()
+    cs = tmp_path / "chrom.sizes"
+    cs.touch()
+    methf = tmp_path / "meth.GC.tsv.gz"
+    methf.touch()
+    rnaf = tmp_path / "rna.tsv"
+    rnaf.touch()
+
+    lp = Linkapy_Parser(
+        methylation_path = str(tmp_path),
+        transcriptome_path = str(tmp_path),
+        output = str(tmp_path),
+        mudata = True,
+        methylation_pattern = ('*GC*',),
+        transcriptome_pattern = ('*tsv',),
+        NOMe = False,
+        threads = 10,
+        chromsizes = (str(cs),),
+        regions = (str(reg),),
+        blacklist = (),
+        binsize = 10000,
+        project = 'linkapy_API'
+    )
